e1fd99ed52b4998a8697b643bda5fc9463ec3864
Ben Juarez <benjuarez@Bens-MacBook-Pro.local>
2021-03-24 21:17:30 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+/// <reference lib="webworker" />
 var schedule = require('node-schedule');
 
 // register first
@@ -7,61 +8,62 @@ var schedule = require('node-schedule');
 //   });
 
 // install the service worker
-var CACHE_NAME = 'test-site-cache-v1';
-var urlsToCache = [
-  '/',
-  '/App.css',
-  '/index.css'
-];
+// var CACHE_NAME = 'test-site-cache-v1';
+// var urlsToCache = [
+//   '/',
+//   '/App.css',
+//   '/index.css'
+// ];
 
-self.addEventListener('install', function(event) {
-  console.log('worker install event in progress...');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  )
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-
-  var cacheAllowlist = ['test-site-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheAllowlist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-
-// if(window.Notification.permission === 'granted') {
-//   console.log('service twerker');
-//   var job = schedule.scheduleJob('*/1 * * * *', function() {
-//     console.log('run job');
-//     var Notification = new Notification('snaps!', {
-//       body: 'meow meow'
+// self.addEventListener('install', function(event) {
+//   console.log('worker install event in progress...');
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then(function(cache) {
+//       console.log('Opened cache');
+//       return cache.addAll(urlsToCache);
 //     })
-//   })
-// }
+//   )
+// });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
+
+// self.addEventListener('activate', function(event) {
+
+//   var cacheAllowlist = ['test-site-cache-v1'];
+
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.map(function(cacheName) {
+//           if (cacheAllowlist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
+self.skipWaiting();
+
+if(window.Notification.permission === 'granted') {
+  console.log('service twerker');
+  var job = schedule.scheduleJob('*/1 * * * *', function() {
+    console.log('run job');
+    var Notification = new Notification('snaps!', {
+      body: 'meow meow'
+    })
+  })
+}
